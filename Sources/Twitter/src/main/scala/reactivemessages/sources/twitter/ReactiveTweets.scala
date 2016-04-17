04f8a0201235de90cfc1cfbd8b3f9c8f36d85206
Aleksandr <4lex1v@gmail.com>
2016-04-16 22:04:34 -0400

    [Twitter] Adjust types with respect to variance changes

--- Sources/Twitter/src/main/scala/reactivemessages/sources/twitter/ReactiveTweets.scala
+++ Sources/Twitter/src/main/scala/reactivemessages/sources/twitter/ReactiveTweets.scala
@@ -5,9 +5,17 @@ import twitter4j._
 
 final class ReactiveTweets(client: TwitterStream) extends ReactiveMessagesSource[Status] {
 
-  override def registerListener(listener: ReactiveMessagesListener[Status]): Unit = {
+  override def registerListener[StatusMessage >: Status](listener: ReactiveMessagesListener[StatusMessage]): Unit = {
     client.addListener(new ReactiveStatusListener(listener))
   }
 
+}
+
+object ReactiveTweets {
+  def stream[StatusMessage >: Status](listener: ReactiveMessagesListener[StatusMessage]): Unit = {
+    val client = clientFromConfig()
+    new ReactiveTweets(client).registerListener(listener)
+    client.sample("en")
+  }
 }
 
