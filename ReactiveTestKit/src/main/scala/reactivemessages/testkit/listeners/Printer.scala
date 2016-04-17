6452ab45c21df346ddb0a773702e0c491d1f2e31
Aleksandr <4lex1v@gmail.com>
2016-04-16 22:04:58 -0400

    [TestKit] Update Printer listener

--- ReactiveTestKit/src/main/scala/reactivemessages/testkit/listeners/Printer.scala
+++ ReactiveTestKit/src/main/scala/reactivemessages/testkit/listeners/Printer.scala
@@ -1,11 +1,11 @@
 package reactivemessages.testkit
 package listeners
 
-import reactivemessages.sources.{Message, ReactiveMessagesListener}
+import reactivemessages.sources.ReactiveMessagesListener
 
 object Printer extends ReactiveMessagesListener[Any] {
 
-  override def onMessage(message: Message[Any]): Unit = println(message)
+  override def onMessage[MT <: Any](message: MT): Unit = println(message)
 
   override def onError(error: Throwable): Unit = throw error
 }
