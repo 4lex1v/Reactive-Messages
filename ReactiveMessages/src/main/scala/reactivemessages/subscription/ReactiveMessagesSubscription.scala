1790d05c875f75808f4daa882582e27f43266ac6
Aleksandr <4lex1v@gmail.com>
2016-04-17 20:37:53 -0400

    [Messages] Hide ReactiveMessagesSubscription

--- ReactiveMessages/src/main/scala/reactivemessages/subscription/ReactiveMessagesSubscription.scala
+++ ReactiveMessages/src/main/scala/reactivemessages/subscription/ReactiveMessagesSubscription.scala
@@ -4,9 +4,13 @@ import akka.actor.ActorRef
 import reactivemessages.internal.Protocol
 import org.reactivestreams.Subscription
 
+private[reactivemessages]
 final class ReactiveMessagesSubscription(actorSubscription: ActorRef) extends Subscription {
-
   override def cancel(): Unit = { actorSubscription ! Protocol.CancelSubscription }
-
   override def request(n: Long): Unit = { actorSubscription ! Protocol.RequestMore(n) }
+}
+
+object ReactiveMessagesSubscription {
+  def apply(subscriptionActor: ActorRef): ReactiveMessagesSubscription =
+    new ReactiveMessagesSubscription(subscriptionActor)
 }
